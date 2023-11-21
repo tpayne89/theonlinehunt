870370280d1e826515a481592075cc726dc7f3d1
tpayne89 <86229366+tpayne89@users.noreply.github.com>
2023-11-21 23:03:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -67,5 +67,34 @@ function showLeaderboard() {
     window.location.href = 'leaderboard.html';
 }
 
+// Dummy data for demonstration purposes
+const leaderboardData = [
+    { name: 'Player 1', position: 1 },
+    { name: 'Player 2', position: 2 },
+    // Add more entries as needed
+];
+
+function showLeaderboard() {
+    const outputElement = document.getElementById('output');
+    outputElement.innerHTML = '<h2>Leaderboard</h2>';
+    
+    const leaderboardTable = document.createElement('table');
+    leaderboardTable.id = 'leaderboard-table';
+
+    const tableHead = document.createElement('thead');
+    tableHead.innerHTML = '<tr><th>Player Name</th><th>Position</th></tr>';
+    leaderboardTable.appendChild(tableHead);
+
+    const tableBody = document.createElement('tbody');
+    leaderboardData.forEach((entry) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${entry.name}</td><td>${entry.position}</td>`;
+        tableBody.appendChild(row);
+    });
+    leaderboardTable.appendChild(tableBody);
+
+    outputElement.appendChild(leaderboardTable);
+}
+
 // Initial setup
 showQuestion();
